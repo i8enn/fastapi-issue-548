4557b9eabb5849718ee973ee44978193d439657d
Ivan Galin <gin.volgograd@gmail.com>
2020-02-26 01:31:40 +0400

    Trying reproduce bug a @xgenvn way

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,7 @@
 """Run with python module"""
 
 if __name__ == '__main__':
-    from app.commands import cli
-    cli()
+    import uvicorn
+    from .main import create_app
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
+    create_app()
--- app/api/users/auth.py
+++ app/api/users/auth.py
@@ -1,5 +1,5 @@
 """Endpoints for auth user API"""
-from fastapi import Body, APIRouter
+from fastapi import APIRouter, Form
 from starlette.status import HTTP_200_OK
 
 
@@ -12,11 +12,8 @@ auth_routes = APIRouter()
 )
 def password_reset(
         *,
-        reset_token: str,
         # Not working with password `embed=True` and uvicorn without --reload
-        password: str = Body(
-            ..., title='New password', embed=True, min_length=3, max_length=20
-        )
+        password: str = Form(...)
 ):
     """Recovery account (reset password) by reset token"""
     pass
